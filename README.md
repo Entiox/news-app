9c4d4306b954f31f563ebf6361ab7d1ada342878
Stakljar <drazenantunovic012@gmail.com>
2023-01-11 00:43:15 +0100

    Completed project

---
+++ README.md
@@ -0,0 +1,3 @@
+App that enables you to watch various news and add preferences.
+
+REST API used from https://mediastack.com
---
+++ app/src/main/java/agency/five/codebase/android/newsapp/notification/NotificationWorker.kt
@@ -0,0 +1,57 @@
+package agency.five.codebase.android.newsapp.notification
+
+import agency.five.codebase.android.newsapp.MainActivity
+import agency.five.codebase.android.newsapp.R
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+
+class NotificationWorker(private val appContext: Context, workerParams: WorkerParameters) :
+    CoroutineWorker(appContext, workerParams) {
+
+    override suspend fun doWork(): Result {
+        createNotification()
+        return Result.success()
+    }
+
+    private fun createNotification() {
+        val intent = Intent(appContext, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(
+                appContext,
+                0,
+                intent,
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
+            )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationManagerCompat.from(appContext).notify(
+                1, NotificationCompat.Builder(appContext, "NEWS_01")
+                    .setSmallIcon(R.drawable.ic_newspaper)
+                    .setContentTitle("Check out news")
+                    .setContentText("Enter NewsApp to keep up with variety of news.")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(pendingIntent)
+                    .build()
+            )
+        }
+        else {
+            NotificationManagerCompat.from(appContext).notify(
+                1, NotificationCompat.Builder(appContext)
+                    .setSmallIcon(R.drawable.ic_newspaper)
+                    .setContentTitle("Check out news")
+                    .setContentText("Enter NewsApp to keep up with variety of news.")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(pendingIntent)
+                    .build()
+            )
+        }
+    }
+}
